7f39f1fffd1e0ba6139f4d61d8d9f05da9d19f3b
WaWaLwin <wwl@mit.com.mm>
2023-04-26 14:36:30 +0630

    testing

--- CustomApi/Api/CustomInterface.php
+++ CustomApi/Api/CustomInterface.php
@@ -2,39 +2,17 @@
 
 namespace MageDelight\CustomApi\Api;
 
+use MageDelight\CustomApi\Api\Data\CustomDataInterface;
+
 interface CustomInterface
 {
     /**
      * GET for Post api
-     * @param \MageDelight\CustomApi\Model\Data\CustomData $customData
+     * @param CustomDataInterface $customData
      * @return string
      */
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public function getPost(\MageDelight\CustomApi\Model\Data\CustomData $customData);
-=======
-=======
->>>>>>> parent of 362076b ( ok now for checking one parameter and its value with if conditions such as if it is lower or higher than this particular number, what message will show etc)
-    public function getPost($value1,$value2);
-    //public function getPost($value);
-    //public function getPost();
->>>>>>> parent of 362076b ( ok now for checking one parameter and its value with if conditions such as if it is lower or higher than this particular number, what message will show etc)
+    public function getPost(CustomDataInterface $customData);
 }
-
-
-// namespace MageDelight\CustomApi\Api;
-
-// use MageDelight\CustomApi\Api\Data\CustomDataInterface;
-
-// interface CustomInterface
-// {
-//     /**
-//      * GET for Post api
-//      * @param CustomDataInterface $customData
-//      * @return string
-//      */
-//     public function getPost(CustomDataInterface $customData);
-// }
 
 
 
